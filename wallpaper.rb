851970d294d1a9e51d4110c108751d90e0b5f3cb
Björn Husberg <bjorn.husberg@valtech.se>
2014-04-22 08:39:03 +0200

    Expanding file names

--- wallpaper.rb
+++ wallpaper.rb
@@ -7,12 +7,13 @@ unless ARGV.length == 1
   exit
 end
 
+filename = File.expand_path(ARGV[0])
 
 DB_LOC = File.join(ENV["HOME"], "Library/Application Support/Dock/desktoppicture.db")
 DB = SQLite3::Database.new(DB_LOC)
 
-DB.execute("SELECT ROWID,value FROM data").each do |(rowid,path)|
-  DB.execute("UPDATE data SET value=? WHERE ROWID=?",[ARGV[0],rowid])  
+DB.execute("SELECT ROWID,value FROM data").each do |(rowid, path)|
+  DB.execute("UPDATE data SET value=? WHERE ROWID=?", [filename, rowid])
 end
 
 `killall Dock`
